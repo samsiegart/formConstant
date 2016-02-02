f1c4d088ae814800b815f156bb2f510e988c358d
samsiegart <srsiegar@buffalo.edu>
2016-02-02 18:43:28 -0500

    big overhaul

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function() {
+  console.log('App listening on port 3000!');
+});
